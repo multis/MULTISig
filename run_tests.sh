df1f0c148b85d8092e94d10d488e2953221a1b72
Nizar S <nisehl@gmail.com>
2019-12-30 17:21:56 +0100

    add comments for run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+# running 'truffle test' does compile the contracts each time
+# to avoid duplicate compilation for our 2 tests, 'truffle compile' does the trick
+# this compiles the contracts for both tests once
 truffle compile
 
+# start an instance of ganache-cli, trap it in a background job
+# redirect its output to /dev/null and kill it on exit
 trap 'kill $!' EXIT
 ganache-cli --allowUnlimitedContractSize --gasLimit=97219750 > /dev/null &
 
